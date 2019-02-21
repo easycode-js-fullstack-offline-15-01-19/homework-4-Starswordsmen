a44cb9a1290b9b335ec5828fef86cec3609c6b5d
Starswordsmen <saltansergei@gmail.com>
2019-02-21 11:22:33 +0200

    Салтан, 4 дз

--- homework.js
+++ homework.js
@@ -1,2 +1,124 @@
 // -------------------------- Home work --------------------------
 // -------------------------- ФИО --------------------------
+// Салтан Сергей
+// 1
+// function multiply(){
+// if (multiply() = `number`) 
+//     return console.log(`number` * `number`)
+// else (multiply() = '') 
+//     return console.log(multiply() = 0);
+// }
+// 2
+// function str() {
+//     switch (newStr) {
+//         case str() = `string`:
+//             " "
+//             break;
+//     }
+//     return (`string`.reverseString);
+// }
+
+// function dctStrange($dorm = `string`){
+// console.log($dorm.reverseString())
+// }
+
+
+
+// 3
+// function youngStr() {
+//     let str = 'string';
+//     let strNew = str.charCodeAt(`string`);
+// return strNew.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '); 
+// }
+
+// 4
+// function getRandom(min, max) {
+//   let nbm = Math.random();
+//   if(typeof min, max >= 10 || > 1 ) return console.log(`error`),
+//   else (typeof  min, max >= 10 || >= 1 != nbm) return console.log(`Вы не угадали ваше число ${max, min} а выпало число ${nbm}`),
+//   else if (typeof  min, max >= 10 || >= 1 = nbm) return console.log(`Вы выиграли ${nbm}`),
+//  }
+
+
+
+// 5
+// function accept(`number`){
+//   let numer = [];
+//    for (let i = 1; i <= n; i++) {
+//     arr.push(i);
+// }
+//   return numer;
+// }
+
+
+
+// 6
+// function massive() {
+//     var myArray = [1,2,3,4];
+//    // console.log(myArray);
+//    return console.log( myArray).doubleArray;
+// }
+
+// 7
+// function getArray(){
+//     var someArrays = [`numbers`] || [' ',];
+//     console.log(someArrays.changeCollection)
+//     return console.log(someArrays);
+// }
+// 8
+
+// let users = [
+//     {
+//     name: 'Denis',
+//     age: '29',
+//     gender: 'male'
+// },
+// {
+//     name: 'Ivan',
+//         age: '20',
+//             gender: 'male'
+// }];
+
+//     function funcGetUsers(arr, key, value) {
+//         if (!key || !value) {
+//             return 'Error, enter all options';
+//         }
+
+//         let newArr = [];
+//         for (let i = 0; i < arr.length; i++) {
+//             if (key in arr[i] && value === arr[i][key]) {
+//                 newArr.push(arr[i]);
+//             }
+//         }
+//         return newArr;
+//     }
+
+
+// 9
+// function firsFunc(arr, callback) {
+//     if (!Array.isArray(arr)) return console.log('error');
+
+//     let res = " New value: ";
+
+//     for (let i = 0; i < arr.length; i++) {
+//         res += callback(arr[i]);
+//     }
+//     return res;
+// }
+
+// let newRes1 = firsFunc(['my', 'name', 'is', 'Trinity'], function (item) {
+//     return item[0].toUpperCase() + item.slice(1);
+// });
+
+// let newRes2 = firsFunc([10, 20, 30], function (item) {
+//     return item * 10 + ', ';
+
+// });
+// let newRes3 = firsFunc([{ age: 45, name: 'Jhon' }, { age: 20, name: 'Aaron' }], function (item) {
+//     return (`${item.name} is ${item.age}, `);
+// });
+
+// let newRes4 = firsFunc(['abc', '123'], function (item) {
+//     // return arr[i].split('').reverse().join('');
+//     return item.split('').reverse().join('') + ', ';
+// });
